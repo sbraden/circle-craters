89b95a840cb376773ff4a410a5845f87613a08e8
Sarah Braden <sarah.braden@hireiqinc.com>
2015-03-03 10:21:57 -0700

    checkpoint

--- circleCraters/circle_craters.py
+++ circleCraters/circle_craters.py
@@ -5,9 +5,9 @@
                                  A QGIS plugin
  A crater counting tool for planetary science
                               -------------------
-        begin                : 2014-12-31
+        begin                : 2015-01-28
         git sha              : $Format:%H$
-        copyright            : (C) 2014 by Sarah E Braden
+        copyright            : (C) 2015 by Sarah E Braden
         email                : braden.sarah@gmail.com
  ***************************************************************************/
 
@@ -20,59 +20,96 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, SIGNAL, Qt, QObject
-from PyQt4.QtGui import QAction, QIcon
-from PyQt4.QtGui import QMessageBox
+import os.path
+import math
+import collections
+
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
+from PyQt4.QtGui import QAction, QIcon, QColor
+
+from qgis.core import QgsPoint, QgsGeometry, QgsFeature, QgsVectorLayer, QgsMapLayerRegistry, QgsMapLayer, QgsErrorMessage
+from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand, QgsMessageBar
+
 # Initialize Qt resources from file resources.py
-import resources
+import resources  # noqa
+
 # Import the code for the dialog
 from circle_craters_dialog import CircleCratersDialog
-import os.path
-import rectangle_example
-
-from qgis.gui import *
-from qgis.core import *
 
 
-class PointTool(QgsMapTool):
-    def __init__(self, canvas):
-        QgsMapTool.__init__(self, canvas)
-        self.canvas = canvas
-
-    def canvasPressEvent(self, event):
-        pass
-
-    # def canvasMoveEvent(self, event):
-    #     x = event.pos().x()
-    #     y = event.pos().y()
-    #     point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
-
-    def canvasReleaseEvent(self, event):
-        #Get the click
-        x = event.pos().x()
-        y = event.pos().y()
-        print x
-        print y
-        point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
-        print point
-
-    def activate(self):
-        pass
-
-    def deactivate(self):
-        pass
-
-    def isZoomTool(self):
-        return False
-
-    def isTransient(self):
-        return False
-
-    def isEditTool(self):
-        return True
+Point = collections.namedtuple('Point', ['x', 'y'])
 
 
-class CircleCraters:
+class Circle(object):
+    """ A cicle is defined by three distinct non-colinear points."""
+
+    def __init__(self, points):
+        """Input: a list of tuples
+        """
+        self.a, self.b, self.c = points
+        print repr(self)
+        self.compute_deltas()
+        self.points_too_close()
+        self.compute_line_slopes()
+        self.check_if_colinear()
+
+    def compute_deltas(self):
+        # TODO: If the points are colinear, x_delta_a will be zero and
+        # compute_line_slopes will throw an ZeroDivisionError
+        self.y_delta_a = self.b.y - self.a.y
+        self.x_delta_a = self.b.x - self.a.x
+        self.y_delta_b = self.c.y - self.b.y
+        self.x_delta_b = self.c.x - self.b.x
+
+    def compute_line_slopes(self):
+        self.a_slope = self.y_delta_a / self.x_delta_a
+        self.b_slope = self.y_delta_b / self.x_delta_b
+
+    def check_if_colinear(self):
+        if abs(self.a_slope - self.b_slope) <= 0.000000001:
+            # TODO: this should be printed as a message to the user in QGIS
+            QgsErrorMessage('The three points are colinear. Cannot compute circle.\n')
+            # TODO: clear the 3 points
+
+    def points_too_close(self):
+        if self.is_small(self.x_delta_a) & self.is_small(self.y_delta_b):
+            # TODO: this should be printed as a message to the user in QGIS
+            QgsErrorMessage('The points are too close together. Cannot compute circle.\n')
+            # TODO: clear the 3 points
+
+    def is_small(self, value):
+        return abs(value) <= 0.000000001
+
+    def radius(self):
+        return self.distance(self.center, self.a)
+
+    def diameter(self):
+        return self.radius() * 2
+
+    def distance(self, point_1, point_2):
+        part_1 = math.pow((point_1.x - point_2.x), 2)
+        part_2 = math.pow((point_1.y - point_2.y), 2)
+        return math.sqrt(part_1 + part_2)
+
+    def get_center(self):
+        # TODO: this function is too long
+
+        part_1 = self.a_slope * self.b_slope * (self.a.y - self.c.y)
+        part_2 = self.b_slope * (self.a.x + self.b.x)
+        part_3 = self.a_slope * (self.b.x + self.c.x)
+
+        center_x = (part_1 + part_2 - part_3) / (2 * (self.b_slope - self.a_slope))
+        a_b_midpoint = Point((self.a.x + self.b.x) / 2, (self.a.y + self.b.y) / 2)
+        center_y = -1 * (center_x - a_b_midpoint.x) / self.a_slope + a_b_midpoint.y
+
+        self.center = Point(center_x, center_y)
+        return self.center
+
+    def __repr__(self):
+        return 'Point(%r, %r, %r)' % (self.a, self.b, self.c)
+
+
+class CircleCraters(object):
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
@@ -85,12 +122,11 @@ class CircleCraters:
         """
         # Save reference to the QGIS interface
         self.iface = iface
-        # a reference to our map canvas (Return a pointer to the map canvas)
         self.canvas = self.iface.mapCanvas()
-        # this QGIS tool emits as QgsPoint after each click
-        self.clickTool = QgsMapToolEmitPoint(self.canvas)
+
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
+
         # initialize locale
         locale = QSettings().value('locale/userLocale')[0:2]
         locale_path = os.path.join(
@@ -111,9 +147,24 @@ class CircleCraters:
         # Declare instance attributes
         self.actions = []
         self.menu = self.tr(u'&Circle Craters')
-        # TODO: We are going to let the user set this up in a future iteration
+
         self.toolbar = self.iface.addToolBar(u'CircleCraters')
         self.toolbar.setObjectName(u'CircleCraters')
+
+        self.tool = QgsMapToolEmitPoint(self.canvas)
+        self.tool.canvasClicked.connect(self.handle_click)
+        self.tool.deactivated.connect(self.reset_clicks)
+        self.clicks = []
+
+        self.rb = QgsRubberBand(self.canvas, True)
+        self.rb.setBorderColor(QColor(255, 0, 0))
+        self.rb.setFillColor(QColor(0, 0, 0, 0))
+        self.rb.setWidth(1)
+
+        # TODO: We are going to let the user set this up in a future iteration
+        # This is where the layer is being created
+        self.layer = QgsVectorLayer('polygon', 'layer', 'memory')
+        QgsMapLayerRegistry.instance().addMapLayer(self.layer)
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -140,7 +191,7 @@ class CircleCraters:
         add_to_toolbar=True,
         status_tip=None,
         whats_this=None,
-        parent=None,
+        parent=None
     ):
         """Add a toolbar icon to the toolbar.
 
@@ -204,26 +255,23 @@ class CircleCraters:
 
         return action
 
-    def initGui(self):
+    def initGui(self):  # noqa
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/CircleCraters/icon.png'
+        icon_path = ':/plugins/CircleCraters/3points.png'
         self.add_action(
             icon_path,
-            text=self.tr(u'A crater counting tool for planetary science'),
-            callback=self.run,
-            parent=self.iface.mainWindow()
+            text=self.tr(u'Circle Craters'),
+            callback=self.set_tool,
+            parent=self.iface.mainWindow(),
         )
-        # A good way to connect to signals in PyQt is using this syntax:
-        # This line works!
-        print self.clickTool.canvasClicked.connect(self.handleMouseDown)
-        # rectangle_example.RectangleMapTool(self.canvas)
 
-    def handleMouseDown(self, point, button):
-        """signal canvasClicked() emits a QgsPoint. handleMouseDown() prints
-        out the point???
-        """
-        print str(point.x()) + " , " + str(point.y())
+        self.add_action(
+            ':/plugins/CircleCraters/export.png',
+            text=self.tr(u'Export Data'),
+            callback=self.export_tool,
+            parent=self.iface.mainWindow(),
+        )
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
@@ -233,52 +281,75 @@ class CircleCraters:
                 action)
             self.iface.removeToolBarIcon(action)
 
-    def run(self):
+    def reset_clicks(self):
+        self.clicks = []
+
+    def handle_click(self, point, button):
+        self.clicks.append(Point(point.x(), point.y()))
+
+        if len(self.clicks) != 3:
+            return
+
+        circle = Circle(self.clicks)
+
+        # must be called in this order
+        center = circle.get_center()
+        radius = circle.radius()
+
+        # radius is set to 50
+        self.draw_circle(center.x, center.y, radius)
+
+        msg = "clicks: {}".format(self.clicks)
+        self.iface.messageBar().pushMessage("Info", msg)
+        self.canvas.unsetMapTool(self.tool)
+
+    def set_tool(self):
         """Run method that performs all the real work"""
-        # show the dialog
+        self.canvas.setMapTool(self.tool)
+
+    def write_file(self, directory, filename):
+        if os.path.exists(directory):
+            print 'YAY'
+
+    def export_tool(self):
+        """ Run method that exports data to a file"""
         self.dlg.show()
-        # make our clickTool the tool that we'll use for now:
-        # This line works:
-        self.canvas.setMapTool(self.clickTool)
-
-        # layers = QgsMapLayerRegistry.instance().mapLayers().values()
-
-        # for layer in layers:
-        #     if layer.type() == QgsMapLayer.VectorLayer:
-        #         # User selects the layer that the crater polygons go into
-        #         self.dlg.layerCombo.addItem(layer.name(), layer)
+        # Fetch all loaded layers
+        layers = QgsMapLayerRegistry.instance().mapLayers().values()
+        for layer in layers:
+            if layer.type() == QgsMapLayer.VectorLayer:
+                # Add these layers to the combobox (dropdown menu)
+                self.dlg.craterLayer.addItem(layer.name(), layer)
+                self.dlg.areaLayer.addItem(layer.name(), layer)
 
         # Run the dialog event loop
         result = self.dlg.exec_()
         # See if OK was pressed
-        if result:
-            # Count the features in the vector layer previously selected
-            # index = self.dlg.layerCombo.currentIndex()
-            # layer = self.dlg.layerCombo.itemData(index)
+        if result == 1:
+            crater_index = self.dlg.craterLayer.currentIndex()
+            crater_layer = self.dlg.craterLayer.itemData(crater_index)
+            area_index = self.dlg.areaLayer.currentIndex()
+            area_layer = self.dlg.areaLayer.itemData(area_index)
+            self.write_file(self.dlg.editDirectory.text(), self.dlg.editFilename.text())
+            print 'Crater Layer name:', crater_layer.name()
+            print 'Area Layer name:', area_layer.name()
 
-            # map_point = self.clickTool.toMapCoordinates(point)
+    def get_area(self):
+        return NotImplementedError
 
-            # connecting a slot to ClickTool canvasClicked() signal will let
-            # you implement custom behaviour for the passed in point.
-            # print SIGNAL("canvasClicked(const QgsPoint &, Qt::MouseButton)")
+    def generate_circle(self, x, y, r):
+        segments = 64
+        thetas = [(2 * math.pi) / segments * i for i in xrange(segments)]
+        points = [Point(r * math.cos(t), r * math.sin(t)) for t in thetas]
+        polygon = [QgsPoint(p.x + x, p.y + y) for p in points]
+        return QgsGeometry.fromPolygon([polygon])
 
-            # result = self.mouse_click_makes_point()
+    def draw_circle(self, x, y, r):
+        geometry = self.generate_circle(x, y, r)
 
-            # QMessageBox.information(
-            #     self.iface.mainWindow(),
-            #     "Info",
-            #     "connect = %s" % str(result),
-            # )
-            pass
+        feature = QgsFeature()
+        feature.setGeometry(geometry)
 
-
-
-    # this QGIS tool emits as QgsPoint after each click (is a constructor)
-    # self.clickTool = QgsMapToolEmitPoint(self.canvas)
-
-    # signal canvasClicked() emits a QgsPoint.
-
-    # Get three points from where the user clicks in x, y coordinates
-    # point1 = QgsPoint(startPoint.x(), startPoint.y())
-    # point2 = QgsPoint(startPoint.x(), endPoint.y())
-    # point3 = QgsPoint(endPoint.x(), endPoint.y())
+        self.layer.addFeature(feature)
+        self.rb.setToGeometry(geometry, None)
+        print 'Circle should be there.'
