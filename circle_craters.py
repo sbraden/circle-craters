4bae449ea9154f25068a335b08935a47f85ec08a
Sarah Braden <sarah.braden@hireiqinc.com>
2015-03-15 18:04:59 -0500

    removed license info

--- circle_craters.py
+++ circle_craters.py
@@ -10,15 +10,6 @@
         copyright            : (C) 2015 by Sarah E Braden
         email                : braden.sarah@gmail.com
  ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 """
 import os.path
 import datetime
